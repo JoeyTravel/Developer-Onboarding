069f67abc8ccb3bda7cbc41d2955c2a04690f31e
Vuk Dukic <vuk.dukic@clara.com>
2017-03-28 17:19:31 -0700

    Update EmbeddingComponents.js

--- EmbeddingComponents.js
+++ EmbeddingComponents.js
@@ -6,3 +6,19 @@ var Badge = React.createClass({
   }
 });
 
+var Form = React.createClass({
+    render: function() {
+      return <div>
+        <Badge title={this.props.title} number={this.props.number} />
+      </div>
+    }
+});
+
+// Create Form
+
+var = options + {
+  title: 'Inbox',
+  number: 5
+}
+React.createElement(Form, options)
+
